3aff928286a8367efffdfe621c66b8d81783e641
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2022-05-26 18:31:03 +1000

    price range and ranking working on the list display from API

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -10,11 +10,19 @@ const PlaceDetails = ({place}) => {
         <Card elevation={6}>
             <CardMedia 
                 style={{height:350}}
-                image={place.photo? place.photo.images.large.url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.theguardian.com%2Ffood%2F2022%2Fapr%2F15%2Flisboeta-london-w1-the-custard-tasted-like-bacony-trifle-restaurant-review&psig=AOvVaw3RBL4l4WV28RGWsrf-8ecv&ust=1653639107207000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPjUqu_b_PcCFQAAAAAdAAAAABAJ'}
+                image={place.photo? place.photo.images.large.url: 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
                 title={place.name}
             /> 
             <CardContent>
                 <Typography gutterBottom variant="h6">{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
             </CardContent>
         </Card>
     )
