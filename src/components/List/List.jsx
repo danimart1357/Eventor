4c79dd7ef52350f61577fb7b0453609f811e797e
Daniel Martinez <danielmartinez@daniels-mbp.lan>
2022-05-28 11:55:12 +1000

    fixed up some styling

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -23,32 +23,24 @@ const List = ({places , childClicked, isLoading, type, setType, rating, setRatin
                
             ) : (
             <>
-           <Box display="flex" justifyContent="center" margin="25px">
-            <Box sx={{ width: 150 }}>
-                <InputLabel>Category</InputLabel>
-                <FormControl className={classes.FormControl}>    
-                    <Select value={type} onChange={(e)=> setType(e.target.value)}>
-                        <MenuItem value='restaurants'>Restaurants</MenuItem>
-                        <MenuItem value='hotels'>Hotels</MenuItem>
-                        <MenuItem value='attractions'>Entertainment</MenuItem>
-                    </Select>
-                </FormControl>
-            </Box>
-            <FormControl className={classes.FormControl}>
-                <Box sx={{ width: 150 }}>
-                <InputLabel className={classes.ratingLabel}>Minimum Rating</InputLabel>
-                    <Slider
-                    onChange={(e) => setRating(e.target.value)}
-                    className={classes.ratingSlider}
-                    defaultValue={1}
-                    valueLabelDisplay="auto"
-                    step={1}
-                    min={1}
-                    max={5}
-                    />
-                </Box>
-            </FormControl>
-           </Box>
+            <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          
            <Grid container spacing={3} className={classes.list}>
                {places?.map((place, i)=>(
                    <Grid  item key={i} xs={12}>
